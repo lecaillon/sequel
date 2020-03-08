8c55d1c995f6340c72df302ed14865bc29b2893f
Philippe Lécaillon <lecaillon@hotmail.com>
2020-03-08 16:39:34 +0100

    First back MVP

---
+++ back/Sequel/src/Sequel/Controllers/SequelController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Sequel.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SequelController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public SequelController(ILogger<SequelController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("ping")]
+        public string Ping()
+        {
+            return "pong";
+        }
+    }
+}
