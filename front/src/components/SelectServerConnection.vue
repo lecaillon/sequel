dbaee3b781284ee2ea66f95ced4bcb2cef403926
Philippe Lécaillon <lecaillon@hotmail.com>
2020-03-14 15:37:07 +0100

    Design component

--- front/src/components/SelectServerConnection.vue
+++ front/src/components/SelectServerConnection.vue
@@ -1,20 +1,27 @@
 <template>
-      <v-autocomplete
-        placeholder="Select a server"
-        auto-select-first="false"
-        dense
-        hide-details
-        clearable
-        solo
-        :items="servers"
-        item-text="connectionString"
-      ></v-autocomplete>
+  <v-autocomplete
+    placeholder="Select a server"
+    auto-select-first="false"
+    dense
+    hide-details
+    clearable
+    solo
+    :items="servers"
+  >
+    <template v-slot:selection="{ attrs, item }">
+      <v-chip label small color="primary">{{ item.environment }}</v-chip>
+      <span class="ms-3">{{ item.name }}</span>
+    </template>
+    <template v-slot:item="{ index, item }">
+      <v-chip label small color="primary">{{ item.environment }}</v-chip>
+      <span class="ms-3">{{ item.name }}</span>
+    </template>
+  </v-autocomplete>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import store from '@/store';
-import { ServerConnection } from "@/models/serverConnection";
+import store from "@/store";
 
 export default Vue.extend({
   name: "SelectServerConnection",
