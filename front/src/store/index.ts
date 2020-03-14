a1a847e21f1a43ef356dd4d0d1a245ecb935d347
Philippe Lécaillon <lecaillon@hotmail.com>
2020-03-14 13:01:05 +0100

    Code style

--- front/src/store/index.ts
+++ front/src/store/index.ts
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import { http } from '@/core/http';
-import { ServerConnection } from '@/models/serverConnection';
+import { http } from "@/core/http";
+import { ServerConnection } from "@/models/serverConnection";
 
 Vue.use(Vuex);
 
@@ -10,7 +10,7 @@ export default new Vuex.Store({
     servers: [] as ServerConnection[]
   },
   actions: {
-    fetchServers: async (context) => {
+    fetchServers: async context => {
       const servers = await http.get<ServerConnection[]>("http://localhost:5000/sequel/server-connection");
       context.commit("setServers", servers);
     }
