e5fc059f3f09f65fb9b1a5d621ca19c148f39d2a
Philippe Lécaillon <lecaillon@hotmail.com>
2020-03-18 19:58:14 +0100

    Fix snackbar always visible

--- front/src/components/AppSnackbar.vue
+++ front/src/components/AppSnackbar.vue
@@ -1,15 +1,24 @@
 <template>
-  <v-snackbar v-model="show" :color="color" :timeout="6000" vertical bottom right>
+  <v-snackbar
+    :value="show"
+    :color="color"
+    :timeout="6000"
+    @input="$emit('close')"
+    vertical
+    bottom
+    right
+  >
     {{ message }}
     <ul v-if="details">
       <li v-for="(detail, index) in details" :key="index">{{ detail }}</li>
     </ul>
-    <v-btn text @click="show = false">Close</v-btn>
+    <v-btn text @click="$emit('close')">Close</v-btn>
   </v-snackbar>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import store from "@/store";
 
 export default Vue.extend({
   name: "AppSnackbar",
