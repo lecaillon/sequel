87f0784ebd82e809ba911e041840233b4a15d24d
Philippe Lécaillon <lecaillon@hotmail.com>
2020-04-02 21:28:15 +0200

    Exclude monaco-editor features

--- front/vue.config.js
+++ front/vue.config.js
@@ -5,8 +5,49 @@ module.exports = {
   configureWebpack: {
     plugins: [
       new MonacoEditorPlugin({
-        languages: ['sql']
+        languages: ['sql'],
+        features: [
+          '!accessibilityHelp',
+          '!bracketMatching',
+          'caretOperations',
+          'clipboard',
+          '!codeAction',
+          '!codelens',
+          '!colorDetector',
+          'comment',
+          'contextmenu',
+          'coreCommands',
+          'cursorUndo',
+          '!dnd',
+          'find',
+          '!folding',
+          'fontZoom',
+          '!format',
+          '!gotoError',
+          'gotoLine',
+          '!gotoSymbol',
+          '!hover',
+          '!inspectTokens',
+          'linesOperations',
+          '!links',
+          'multicursor',
+          '!parameterHints',
+          '!quickCommand',
+          '!quickOutline',
+          '!referenceSearch',
+          '!rename',
+          '!smartSelect',
+          'snippets',
+          '!suggest',
+          '!toggleHighContrast',
+          '!toggleTabFocusMode',
+          '!transpose',
+          'wordHighlighter',
+          '!wordOperations',
+          '!wordPartOperations']
       })
     ]
   }
 };
+
+// 'caretOperations'
